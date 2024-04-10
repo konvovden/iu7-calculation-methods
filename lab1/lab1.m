15bf2e46231165dcc88a26a2926d2521cfb7c71f
Vladimir Konovalenko <vladimir.konovalenko@qoollo.com>
2024-04-10 19:06:11 +0300

    Fixes

--- lab1/lab1.m
+++ lab1/lab1.m
@@ -1,34 +1,38 @@
 function lab1()
     debug = 1;
+    delaySeconds = 1.0;
 
     a = 0;
     b = 1;
     e = 0.01;
 
-    [x, y, N] = bitwiseSearch(a, b, e, debug);
-   
     fplot(@f, [a, b]);
     hold on;
+
+
+    [x, y, N] = bitwiseSearch(a, b, e, debug, delaySeconds);  
 
     fprintf('RESULT: e = %f | N = %d | x* = %.10f | f(x*) = %.10f', e, N, x, y);
 
     scatter(x, y, 'b', 'filled');
+    
+    hold off;
 end
 
 function y = f(x)
     y = sin((power(x, 4) + power(x, 3) - 3 * x + 3 - power(30, 1/3)) / 2) + tanh((4 * sqrt(3) * power(x, 3) - 2 * x - 6 * sqrt(2) + 1) / (-2 * sqrt(3) * power(x, 3) + x + 3 * sqrt(2))) + 1.2;
 end
 
-function [x, y, N] = bitwiseSearch(a, b, e, debug)
+function [x, y, N] = bitwiseSearch(a, b, e, debug, delaySeconds)
     i = 0;
     x0 = a;
     f0 = f(x0);
     delta = (b - a) / 4;
 
     if debug
-        hold on;
         fprintf('%d: x%d = %.10f | y%d = %.10f\n', 0, 0, x0, 0, f0)
         scatter(x0, f0);
+        pause(delaySeconds);
     end
 
     while 1
@@ -41,6 +45,7 @@ function [x, y, N] = bitwiseSearch(a, b, e, debug)
         if debug
             fprintf('%d: x%d = %.10f | y%d = %.10f\n', i, i, x1, i, f1)
             scatter(x1, f1);
+            pause(delaySeconds);
         end
 
         if f0 <= f1 || x1 <= a || x1 >= b
